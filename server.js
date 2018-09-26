7fb884f006039b35168476c039fcb9325a373570
Marko Gusic <markogusic@aol.com>
2018-09-25 22:26:53 -0700

    working code

---
+++ server.js
@@ -0,0 +1,27 @@
+//dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//data base
+var friends = require("./app/data/friends.js");
+
+//setup for express
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//in the public folder available
+app.use(express.static('app/public'));
+
+//express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+// start server on port number
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
